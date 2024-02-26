b0c5e5a0410e98b3b9587d0dc5972f78b4a2b70f
Liyi Bai <bailiyi2011@126.com>
2024-02-26 14:21:16 +0800

    update input

--- test/replace.sh
+++ test/replace.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp job.json ./BondOrderAnalysis/dpmd_run_script
-#cp job.json ./Piezoelectric/script
+cp job.json ./AvalancheDynamics/dpmd_run_script/equ
+
+cp job.json ./AvalancheDynamics/dpmd_run_script/add_Ef
 
 echo "All job.json files have been replaced."
